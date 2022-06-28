00085262ee1d2a9621009ae827ac336ad76f6144
vik.zak <git@azbukait.ru>
2022-06-28 17:00:56 +0300

    - небольшая оптимизация кода и устранение ошибок

--- app/src/main/java/ru/gb/osmmap/MainActivity.kt
+++ app/src/main/java/ru/gb/osmmap/MainActivity.kt
@@ -24,8 +24,8 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import ru.gb.osmmap.databinding.ActivityMainBinding
 
 
-private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-private lateinit var mapView: MapView;
+private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+private lateinit var mapView: MapView
 private lateinit var binding: ActivityMainBinding
 private lateinit var mapController: IMapController
 
@@ -37,6 +37,7 @@ var setStartGeoPoint: GeoPoint? = null
 
 const val KEY_FILE_SETTING = "MAP_SETTING"
 const val KEY_SET_ZOOM = "SET_ZOOM"
+const val DEFAULT_ZOOM = "4.0"
 const val KEY_LAST_LOCATIONS_LAT = "LAST_LOCATIONS_LAT"
 const val KEY_LAST_LOCATIONS_LON = "LAST_LOCATIONS_LON"
 
@@ -51,46 +52,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        sharedPreferences = getSharedPreferences(KEY_FILE_SETTING, Context.MODE_PRIVATE)
 
         mapView = binding.mapOsmDroid
-        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
         mapController = mapView.controller
 
         // масштабирование и жесты
-        mapView.setBuiltInZoomControls(true);
-        mapView.setMultiTouchControls(true);
+        mapView.setBuiltInZoomControls(true)
+        mapView.setMultiTouchControls(true)
 
         // функции
-        sharedPreferences()
         myLocationFun(mapController)
         rotateMap()
         scaleBarOverlay(true)
         miniMapOverlay(true)
         cpmpasOverlay(true)
+        sharedPreferences()
 
         binding.myLocationButton.setOnClickListener {
             myLocationFun(mapController)
-            sharedPreferences()
         }
-        //val startPoint = GeoPoint(55.755864, 37.617698)
     }
 
     fun sharedPreferences(){
-        //init share pref
-        sharedPreferences = getSharedPreferences(KEY_FILE_SETTING, Context.MODE_PRIVATE)
-
         setZoomSharedPreferences = sharedPreferences.getString(KEY_SET_ZOOM, "")
-        if ((setZoomSharedPreferences == null) || (setZoomSharedPreferences == "")) {
+        if (setZoomSharedPreferences.isNullOrBlank()) {
             sharedPreferencesEditor = sharedPreferences.edit()
-            sharedPreferencesEditor.putString(KEY_SET_ZOOM, "4.0")
+            sharedPreferencesEditor.putString(KEY_SET_ZOOM, DEFAULT_ZOOM)
             sharedPreferencesEditor.apply()
-            mapController.setZoom(4.0)
-
+            mapController.setZoom(DEFAULT_ZOOM.toDouble())
         } else {
-            var getSetZoomSharedPreferences = sharedPreferences.getString(KEY_SET_ZOOM, "")
+            val getSetZoomSharedPreferences = sharedPreferences.getString(KEY_SET_ZOOM, "")
             mapController.setZoom(getSetZoomSharedPreferences!!.toDouble())
         }
-
+        // координаты
         setLastLocationLatSharedPreferences = sharedPreferences.getString(KEY_LAST_LOCATIONS_LAT, "")
         setLastLocationLonSharedPreferences = sharedPreferences.getString(KEY_LAST_LOCATIONS_LON, "")
         if (setLastLocationLatSharedPreferences.isNullOrBlank() && setLastLocationLonSharedPreferences.isNullOrBlank()) {
@@ -103,22 +99,19 @@ class MainActivity : AppCompatActivity() {
             sharedPreferencesEditor.apply()
             mapController.setCenter(setStartGeoPoint)
         } else {
-            var aLat :Double? = sharedPreferences.getString(KEY_LAST_LOCATIONS_LAT,"")!!.toDouble()
-            var aLon :Double? = sharedPreferences.getString(KEY_LAST_LOCATIONS_LON,"")!!.toDouble()
-            var position = GeoPoint(aLat!!.toDouble(), aLon!!.toDouble())
-//            val aLat1 = 55.755864
-//            val aLon1 = 37.617698
-
+            val aLat : Double = sharedPreferences.getString(KEY_LAST_LOCATIONS_LAT,"")!!.toDouble()
+            val aLon : Double = sharedPreferences.getString(KEY_LAST_LOCATIONS_LON,"")!!.toDouble()
+            val position = GeoPoint(aLat, aLon)
             mapController.setCenter(position)
         }
     }
 
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        sharedPreferencesEditor = sharedPreferences.edit()
+        sharedPreferencesEditor.putString(KEY_SET_ZOOM, mapView.zoomLevelDouble.toString())
+        sharedPreferencesEditor.apply()
         return super.onTouchEvent(event)
-//        sharedPreferencesEditor = sharedPreferences.edit()
-//        sharedPreferencesEditor.putString(KEY_SET_ZOOM, mapView.zoomLevelDouble.toString())
-//        sharedPreferencesEditor.apply()
     }
 
 
@@ -149,8 +142,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun scaleBarOverlay(flag: Boolean) {
         // шкала масштаба
-        val mScaleBarOverlay = ScaleBarOverlay(mapView);
-        mScaleBarOverlay.setCentred(true);
+        val mScaleBarOverlay = ScaleBarOverlay(mapView)
+        mScaleBarOverlay.setCentred(true)
         //play around with these values to get the location on screen in the right place for your application
         //val dm = applicationContext.getResources().getDisplayMetrics()
         //mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 50);
@@ -158,7 +151,7 @@ class MainActivity : AppCompatActivity() {
         mScaleBarOverlay.textPaint.color = ContextCompat.getColor(applicationContext, R.color.red)
         mScaleBarOverlay.setTextSize(50f)
         mScaleBarOverlay.isEnabled = flag
-        mapView.overlays.add(mScaleBarOverlay);
+        mapView.overlays.add(mScaleBarOverlay)
     }
 
     private fun rotateMap() {
@@ -177,26 +170,27 @@ class MainActivity : AppCompatActivity() {
         mLocationOverlay.enableFollowLocation()
         mapController.setCenter(mLocationOverlay.myLocation)
         mapView.overlays.add(mLocationOverlay)
+        sharedPreferences()
     }
 
     override fun onResume() {
         super.onResume()
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
-        var prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        //var prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         sharedPreferences()
-        mapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        mapView.onResume() //needed for compass, my location overlays, v6.0.0 and up
     }
 
     override fun onPause() {
-        super.onPause();
+        super.onPause()
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Configuration.getInstance().save(this, prefs);
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        Configuration.getInstance().save(this, prefs)
         sharedPreferences()
-        mapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        mapView.onPause()  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     override fun onDestroy() {
@@ -210,18 +204,18 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        val permissionsToRequest = ArrayList<String>();
-        var i = 0;
+        val permissionsToRequest = ArrayList<String>()
+        var i = 0
         while (i < grantResults.size) {
-            permissionsToRequest.add(permissions[i]);
-            i++;
+            permissionsToRequest.add(permissions[i])
+            i++
         }
         if (permissionsToRequest.size > 0) {
             ActivityCompat.requestPermissions(
                 this,
                 permissionsToRequest.toTypedArray(),
                 REQUEST_PERMISSIONS_REQUEST_CODE
-            );
+            )
         }
     }
 }
